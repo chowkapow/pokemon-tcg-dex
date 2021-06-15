47b9f668225cfb61125eea2bf48689b9ffb2b9da
Eric Chow <eric.zjp.chow@gmail.com>
2021-06-15 18:33:12 -0500

    Append REACT_APP to secret per docs

--- src/services/pokemonTcg.js
+++ src/services/pokemonTcg.js
@@ -4,7 +4,7 @@ export async function getPokemonTcgData(set) {
   return new Promise(async (resolve, reject) => {
     const res = await fetch(url + '?q=' + set, {
       headers: {
-        'X-Api-Key': process.env.POKEMON_TCG_API_KEY,
+        'X-Api-Key': process.env.REACT_APP_POKEMON_TCG_API_KEY,
       },
     });
     resolve(res.json());
